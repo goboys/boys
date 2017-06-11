736539f5c2f2fb34a187def218fff6be979d56e2
yuxb.yuxb <yuxb@eptok.com>
2017-06-11 10:02:43 +0800

    dev 提交

--- src/main/java/com/boys/action/test.java
+++ src/main/java/com/boys/action/test.java
@@ -3,6 +3,7 @@ package com.boys.action;
 public class test {
 	private void Sysout() {
 		// TODO Auto-generated method stub
-		System.out.print("dev 分支搞得");
+		System.out.print("分支搞得");
+		System.out.print("搞得 dev分支");
 	}
 }
